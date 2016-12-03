288211483f2551efa5b776afee63d73e7f8ead19
James Reeves <jreeves@weavejester.com>
2016-12-03 11:39:37 +0000

    Add 'ref' and 'dependencies' functions

--- src/integrant/core.clj
+++ src/integrant/core.clj
@@ -1,6 +1,23 @@
-(ns integrant.core)
+(ns integrant.core
+  (:refer-clojure :exclude [ref])
+  (:require [com.stuartsierra.dependency :as dep]
+            [clojure.walk :as walk]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defrecord Ref [key])
+
+(defn ref [key]
+  {:pre [(keyword? key) (namespace key)]}
+  (->Ref key))
+
+(defn ref? [x]
+  (instance? Ref x))
+
+(defn- find-refs [v]
+  (cond
+    (ref? v)  (list (:key v))
+    (coll? v) (mapcat find-refs v)))
+
+(defn dependencies [m]
+  (reduce-kv (fn [g k v] (reduce #(dep/depend %1 k %2) g (find-refs v)))
+             (dep/graph)
+             m))
