807023a31223c15b9dcb5ff1906a80e34f74879b
James Reeves <jreeves@weavejester.com>
2016-12-03 12:12:23 +0000

    Add read-string function

--- src/integrant/core.clj
+++ src/integrant/core.clj
@@ -1,6 +1,7 @@
 (ns integrant.core
-  (:refer-clojure :exclude [ref])
+  (:refer-clojure :exclude [ref read-string])
   (:require [com.stuartsierra.dependency :as dep]
+            [clojure.edn :as edn]
             [clojure.walk :as walk]))
 
 (defrecord Ref [key])
@@ -21,3 +22,6 @@
   (reduce-kv (fn [g k v] (reduce #(dep/depend %1 k %2) g (find-refs v)))
              (dep/graph)
              m))
+
+(defn read-string [s]
+  (edn/read-string {:readers {'ref ref}} s))
