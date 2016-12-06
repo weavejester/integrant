453702eb69e32e86b9e975b4fedc8e0427405701
James Reeves <jreeves@weavejester.com>
2016-12-06 16:40:08 +0000

    Add description and URL to project file

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject integrant "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Micro-framework for data-driven architecture"
+  :url "https://github.com/weavejester/integrant"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
