043a8aa7e3b7b35b113b4213c6d92c1f9e259c28
James Reeves <jreeves@weavejester.com>
2016-12-06 17:09:11 +0000

    Fix var warnings in test namespace

--- test/integrant/core_test.clj
+++ test/integrant/core_test.clj
@@ -1,36 +1,36 @@
 (ns integrant.core-test
   (:require [clojure.test :refer :all]
-            [integrant.core :refer :all]))
+            [integrant.core :as ig]))
 
 (def log (atom []))
 
-(defmethod init-key :default [k v]
+(defmethod ig/init-key :default [k v]
   (swap! log conj [:init k v])
   [v])
 
-(defmethod halt-key! :default [k v]
+(defmethod ig/halt-key! :default [k v]
   (swap! log conj [:halt k v]))
 
 (deftest ref-test
-  (is (ref? (ref :foo))))
+  (is (ig/ref? (ig/ref :foo))))
 
 (deftest expand-test
-  (is (= (expand {::a (ref ::b), ::b 1})
+  (is (= (ig/expand {::a (ig/ref ::b), ::b 1})
          {::a 1, ::b 1}))
-  (is (= (expand {::a (ref ::b), ::b (ref ::c), ::c 2})
+  (is (= (ig/expand {::a (ig/ref ::b), ::b (ig/ref ::c), ::c 2})
          {::a 2, ::b 2, ::c 2})))
 
 (deftest init-test
   (reset! log [])
-  (let [m (init {::a (ref ::b), ::b 1})]
+  (let [m (ig/init {::a (ig/ref ::b), ::b 1})]
     (is (= m {::a [[1]], ::b [1]}))
     (is (= @log [[:init ::b 1]
                  [:init ::a [1]]]))))
 
 (deftest halt-test
   (reset! log [])
-  (let [m (init {::a (ref ::b), ::b 1})]
-    (halt! m)
+  (let [m (ig/init {::a (ig/ref ::b), ::b 1})]
+    (ig/halt! m)
     (is (= @log [[:init ::b 1]
                  [:init ::a [1]]
                  [:halt ::a [[1]]]
@@ -39,4 +39,4 @@
 (deftest missing-ref-test
   (is (thrown-with-msg? clojure.lang.ExceptionInfo
                         #"Missing definitions for refs: :integrant.core-test/b"
-                        (init {::a (ref ::b)}))))
+                        (ig/init {::a (ig/ref ::b)}))))
